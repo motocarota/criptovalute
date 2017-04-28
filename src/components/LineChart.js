2863559799fd93bdb866db64e75917e425c8f1e3
Elvis Poggi <poggi.elvis@gmail.com>
2017-04-28 14:10:31 +0200

    assigned correct values to char

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,11 +1,20 @@
 import { VictoryChart, VictoryLine, VictoryTheme } from 'victory'
 import React from 'react'
 
-const LineChart = ({history}) =>
-  <div>
+const LineChart = ({history}) => {
+  const convert = history   // for every history row
+  .map(row => row[0])     // return only the first element
+  .map(ts => new Date(ts))  // convert it to date
+  .map(date => date.toLocaleString())  // pretty output
+  // console.log(convert
+  // .map(str => str.replace(/[^\w\s]/gi, ''))  removes special characters
+  // )
+  console.log(new Date().getDate())
+  return <div>
     <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
-      <VictoryLine data={history} x='0' y='1' />
+      <VictoryLine data={history} x={convert} y='1' />
     </VictoryChart>
   </div>
+}
 
 export default LineChart
