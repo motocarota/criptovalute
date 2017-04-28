8cea72eb92c20b69bdbc425a034a9e3ba8093fd5
Elvis Poggi <poggi.elvis@gmail.com>
2017-04-28 17:21:27 +0200

    minor fix

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -6,13 +6,11 @@ const LineChart = ({history}) => {
   .map(row => row[0])     // return only the first element
   .map(ts => new Date(ts))  // convert it to date
   .map(date => date.toLocaleString())  // pretty output
-  // console.log(convert
-  // .map(str => str.replace(/[^\w\s]/gi, ''))  removes special characters
-  // )
-  console.log(new Date().getDate())
+
+  console.log('convert:', convert)
   return <div>
     <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
-      <VictoryLine data={history} x={convert} y='1' />
+      <VictoryLine data={history} x='0' y='1' />
     </VictoryChart>
   </div>
 }
