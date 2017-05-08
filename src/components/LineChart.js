a2e974c5869c4c8a30b33541e906206f4dda5ae5
Elvis Poggi <poggi.elvis@gmail.com>
2017-05-08 15:02:45 +0200

    updated convert format

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -3,14 +3,15 @@ import React from 'react'
 
 const LineChart = ({history}) => {
   const convert = history   // for every history row
-  .map(row => row[0])     // return only the first element
-  .map(ts => new Date(ts))  // convert it to date
-  .map(date => date.toLocaleString())  // pretty output
+  .map(row => [row[0], row[1]])     // return only the first two elements
+  .map(ts => [new Date(ts[0]), ts[1]])  // convert it to date
+  .map(date => [date[0].toLocaleString(), date[1]])  // pretty output
+  .map(nd => [nd[0].substring(0, nd[0].indexOf(',')), nd[1]]) // mantein only date format dd/mm/yyyy
 
-  console.log('convert:', convert)
+  console.log('convert:', convert, 'history: ', history)
   return <div>
     <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
-      <VictoryLine data={history} x='0' y='1' />
+      <VictoryLine data={convert} x='0' y='1' />
     </VictoryChart>
   </div>
 }
