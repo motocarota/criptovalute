f208a36f48cf11ddaec5a146fb2284eda0d7721e
elvis-poggi <poggi.elvis@gmail.com>
2017-05-08 17:47:51 +0200

    changed axis lable text

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,17 +1,22 @@
-import { VictoryChart, VictoryLine, VictoryTheme } from 'victory'
+import { VictoryChart, VictoryLine, VictoryAxis } from 'victory'
 import React from 'react'
 
-const LineChart = ({history}) => {
+const LineChart = ({ history }) => {
   const convert = history   // for every history row
-  .map(row => [row[0], row[1]])     // return only the first two elements
-  .map(ts => [new Date(ts[0]), ts[1]])  // convert it to date
-  .map(date => [date[0].toLocaleString(), date[1]])  // pretty output
-  .map(nd => [nd[0].substring(0, nd[0].indexOf(',')), nd[1]]) // mantein only date format dd/mm/yyyy
+    .map(row => [row[0], row[1]])  // return only the first two elements
+    .reverse() // order from older to newer
 
   console.log('convert:', convert, 'history: ', history)
-  return <div>
-    <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
-      <VictoryLine data={convert} x='0' y='1' />
+  return <div viewBox='0, 0, width, height' width='100%' height='400px' >
+    <VictoryChart domainPadding={20}>
+      <VictoryAxis tickFormat={(x) => new Date(x).toLocaleDateString()} fixLabelOverlap style={{
+        axis: { stroke: '#756f6a' },
+        axisLabel: { fontSize: 8, padding: 20 },
+        grid: { stroke: (t) => t === 10 ? 'red' : 'grey' },
+        ticks: { stroke: 'grey' },
+        tickLabels: { fontSize: 8, padding: 5 }
+      }} />
+      <VictoryLine data={convert} x={0} y={1} />
     </VictoryChart>
   </div>
 }
